c4b183a8c070cb05d495efe79e952c85971a52c4
Miroslav Kaspar <k@e0.cz>
2017-03-30 12:03:34 +0100

    session.user.role is an empty string if no roles are set.

--- src/users/login.js
+++ src/users/login.js
@@ -14,6 +14,15 @@ module.exports = function (req, res, next) {
         // don't show the password hash in the response
         delete plainUser.password
 
+        // if there are no roles in the role field, it's represented as a 'null',
+        // which would be correct, but would make us to first check if the role
+        // is null and then if role.indexOf('wantedRole') >= 0... everywhere.
+        // So it's better to just set it to string here and we have one IF instead
+        // of two IFs for each role check.
+        if (typeof plainUser.role != String) {
+          plainUser.role = ''
+        }
+
         res.status(200)
         res.body = JSON.stringify({ token: req.sessionID, user: plainUser })
       } else {
