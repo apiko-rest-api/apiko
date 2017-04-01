c47a29bbe612248057d2c6248e729c2b0e649ac5
Miroslav Kaspar <k@e0.cz>
2017-04-01 10:19:41 +0100

    Hotfix: Default role value setting for currently logged in user.

--- src/users/login.js
+++ src/users/login.js
@@ -19,7 +19,7 @@ module.exports = function (req, res, next) {
         // is null and then if role.indexOf('wantedRole') >= 0... everywhere.
         // So it's better to just set it to string here and we have one IF instead
         // of two IFs for each role check.
-        if (typeof plainUser.role != String) {
+        if (typeof plainUser.role != 'string') {
           plainUser.role = ''
         }
 
